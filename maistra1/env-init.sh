022e8dac067b5f3ac8db3769b842ebbd34d00643
Brian Avery <bavery@redhat.com>
2020-05-07 14:17:25 -0400

    update operators

--- maistra1/env-init.sh
+++ maistra1/env-init.sh
@@ -1,5 +1,5 @@
 #install the ServiceMesh operator
-cat <<EOM | ${OC} apply -f -
+cat <<EOM | oc apply -f -
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
@@ -11,4 +11,32 @@ spec:
   name: servicemeshoperator
   source: redhat-operators
   sourceNamespace: openshift-marketplace
+EOM
+
+#install the Kiali operator
+cat <<EOM | oc apply -f -
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: kiali
+  namespace: openshift-operators
+spec:
+  channel: 'stable'
+  installPlanApproval: Automatic
+  name: kialioperator
+  source: redhat-operators
+  sourceNamespace: openshift-marketplace
+EOM
+
+oc create project istio-system
+
+cat <<EOM | oc apply -n istio-system -f -
+apiVersion: maistra.io/v1
+kind: ServiceMeshControlPlane
+metadata:
+  name: minimal-install
+spec:
+    istio:
+        kiali:
+            enabled: true
 EOM
